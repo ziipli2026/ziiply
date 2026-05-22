dfb163f8f51217e9984f981e01a8bc6b48de6a40
ziipli2026 <Niko@trueaim.fi>
2026-05-22 19:57:49 +0300

    Update next.config.ts2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["172.20.10.3"],
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
