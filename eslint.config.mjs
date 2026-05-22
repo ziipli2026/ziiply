0f30dc38a0a27088ece6e618319b9de12d4293ed
ziipli2026 <Niko@trueaim.fi>
2026-05-22 19:42:42 +0300

    Update eslint.config.mjs piste jiias

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
