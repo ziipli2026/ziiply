6a59aae79290b408fd65431d36ebdedbf65470e2
ziipli2026 <Niko@trueaim.fi>
2026-05-26 21:36:11 +0300

    Update ZiiplySearchCard.tsx

--- src/app/components/ziiply/cards/ZiiplySearchCard.tsx
+++ src/app/components/ziiply/cards/ZiiplySearchCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 
 export type SearchMode = "cart" | "single";
 export type SearchPanelKind = "none" | "results" | "compare";
@@ -33,27 +33,13 @@ function cx(...classes: Array<string | false | null | undefined>) {
   return classes.filter(Boolean).join(" ");
 }
 
-const cooperFont = '"CooperBT-Light", serif';
-
-export function ZiiplySearchCard({ value, onChange, mode = "single", onModeChange, onCancel, isValid = true, placeholder = "Etsi kauppoja...", activePanel = "none", comparePanel, notFoundTerms = [], gostaLoading, justiinaLoading, onOpenResults, onOpenCompare, className }: SearchCardProps) {
-  const [chips, setChips] = useState<SearchChip[]>([]);
-  const [inputFocused, setInputFocused] = useState(false);
-
-  useEffect(() => {
-    // päivitetään inputin syöttö arvoksi
-  }, [value]);
-
-  // ... muu liiketoimintalogiikka, nappien hanlointi jne.
-
-  const instructionText = ["Syötä osoite tai postinumero", "kuntomaan"];
-  const loadingText = gostaLoading
-    ? ["Etsii", "hinnanhuojennuksia…"]
-    : justiinaLoading
-      ? ["Etsii", "hinnanhuojennukset"]
-      : loading
-        ? ["Etsii", "ostoksia…"]
-        : ["Etsii", "ostokset"];
-
+function ZiiplySearchCard({
+  value,
+  onChange,
+  placeholder = "Etsi kauppoja...",
+  onOpenCompare,
+  className,
+}: SearchCardProps) {
   return (
     <section
       className={cx(
@@ -61,50 +47,58 @@ export function ZiiplySearchCard({ value, onChange, mode = "single", onModeChang
         className,
       )}
     >
-      {/* Taustakuvio (varjostus ja pallokuvio) */}
-      <div className="pointer-events-none absolute inset-0 rounded-[28px] opacity-45 [background-image:radial-gradient(#d8bd75_1.2px,transparent_1.2px)] [background-size:18px_18px] [box-shadow:inset_0_0_0_3px_#fff9e8]" />
+      {/* Taustakuvio: bg-position-[9px_0] korjaa oikean reunan taustakuvion kohdistusvirheen. */}
+      <div className="pointer-events-none absolute inset-0 rounded-[28px] opacity-45 [background-image:radial-gradient(#d8bd75_1.2px,transparent_1.2px)] [background-size:18px_18px] [background-position:9px_0] [box-shadow:inset_0_0_0_3px_#fff9e8]" />
 
       {/* Hakutoimintojen alue */}
       <div className="relative z-10 grid h-[64px] grid-cols-[minmax(0,1fr)_minmax(220px,286px)] items-start gap-4 px-4">
         {/* Sijainnin syöttökenttä */}
         <div className="flex flex-col">
-          <span className="text-[10px] font-black text-[#746749] uppercase">
+          <span className="text-[10px] font-black uppercase text-[#746749]">
             Sijainti
           </span>
+
           <div className="relative mt-1">
             <button
               type="button"
               className="absolute inset-y-0 left-0 flex items-center justify-center px-2"
               onClick={() => {
                 navigator.geolocation.getCurrentPosition(
-                  (pos) => onChange(`${pos.coords.latitude},${pos.coords.longitude}`),
+                  (pos) =>
+                    onChange(`${pos.coords.latitude},${pos.coords.longitude}`),
                   () => {},
                 );
               }}
             >
               📍
             </button>
+
             <input
               type="text"
-              className="border-[3px] border-[#7a653e] rounded-[12px] pl-9 pr-3 py-1 w-full bg-[#f8f0d8] text-[16px] font-black leading-none"
+              className="w-full rounded-[12px] border-[3px] border-[#7a653e] bg-[#f8f0d8] py-1 pl-9 pr-3 text-[16px] font-black leading-none"
               placeholder={placeholder}
               value={value}
-              onChange={(e) => onChange(e.target.value)}
+              onChange={(event) => onChange(event.target.value)}
             />
           </div>
         </div>
 
         {/* Kaupat-valinta */}
         <div className="flex flex-col">
-          <span className="text-[10px] font-black text-[#746749] uppercase text-right">
+          <span className="text-right text-[10px] font-black uppercase text-[#746749]">
             Kaupat
           </span>
+
           <button
             type="button"
-            className="w-full h-[64px] max-w-[286px] bg-[#f8f0d8] rounded-[12px] shadow-[0_2px_0_rgba(90,70,35,0.18)]"
+            className="h-[64px] w-full max-w-[286px] rounded-[12px] bg-[#f8f0d8] shadow-[0_2px_0_rgba(90,70,35,0.18)]"
             onClick={onOpenCompare}
           >
-            <img src="kaupat-kuvake.png" className="h-full w-full object-cover rounded-[12px]" alt="Kaupat" />
+            <img
+              src="kaupat-kuvake.png"
+              className="h-full w-full rounded-[12px] object-cover"
+              alt="Kaupat"
+            />
           </button>
         </div>
       </div>
@@ -112,18 +106,32 @@ export function ZiiplySearchCard({ value, onChange, mode = "single", onModeChang
       {/* Gösta & Justiina kuvat */}
       <div className="absolute bottom-[-64px] left-[36px] right-[36px] z-[6] grid grid-cols-2 gap-5">
         <div className="relative h-[96px] rounded-t-[34px] border-[4px] border-[#d8bd75] bg-[#fff4d3] shadow-[0_0_0_2px_#fff4cd_inset,0_8px_0_rgba(70,50,24,0.25)]">
-          <span className="absolute mb-5 text-[21px] font-black uppercase tracking-[0.38em] text-[#746749] left-1/2 transform -translate-x-1/2 top-5">
+          <span className="absolute left-1/2 top-5 mb-5 -translate-x-1/2 text-[21px] font-black uppercase tracking-[0.38em] text-[#746749]">
             Gösta
           </span>
-          <img src="gosta.png" className="absolute bottom-[-24px] left-1/2 transform -translate-x-1/2 w-[58px] h-[58px] rounded-[29px] border-[3px] border-[#7a653e] bg-[#f8f0d8]" alt="Gösta" />
+
+          <img
+            src="gosta.png"
+            className="absolute bottom-[-24px] left-1/2 h-[58px] w-[58px] -translate-x-1/2 rounded-[29px] border-[3px] border-[#7a653e] bg-[#f8f0d8]"
+            alt="Gösta"
+          />
         </div>
+
         <div className="relative h-[96px] rounded-t-[34px] border-[4px] border-[#d8bd75] bg-[#fff4d3] shadow-[0_0_0_2px_#fff4cd_inset,0_8px_0_rgba(70,50,24,0.25)]">
-          <span className="absolute mb-5 text-[21px] font-black uppercase tracking-[0.38em] text-[#746749] left-1/2 transform -translate-x-1/2 top-5">
+          <span className="absolute left-1/2 top-5 mb-5 -translate-x-1/2 text-[21px] font-black uppercase tracking-[0.38em] text-[#746749]">
             Justiina
           </span>
-          <img src="justiina.png" className="absolute bottom-[-24px] left-1/2 transform -translate-x-1/2 w-[58px] h-[58px] rounded-[29px] border-[3px] border-[#7a653e] bg-[#f8f0d8]" alt="Justiina" />
+
+          <img
+            src="justiina.png"
+            className="absolute bottom-[-24px] left-1/2 h-[58px] w-[58px] -translate-x-1/2 rounded-[29px] border-[3px] border-[#7a653e] bg-[#f8f0d8]"
+            alt="Justiina"
+          />
         </div>
       </div>
     </section>
   );
 }
+
+export { ZiiplySearchCard };
+export default ZiiplySearchCard;
