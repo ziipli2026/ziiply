43c7bf7e4ab759d347cfac7fbf8757331f9fef1d
ziipli2026 <Niko@trueaim.fi>
2026-06-04 04:47:50 +0300

    Update ziiplyStoreDirectory.ts

--- src/app/components/ziiply/offerSearch/ziiplyStoreDirectory.ts
+++ src/app/components/ziiply/offerSearch/ziiplyStoreDirectory.ts
@@ -1,1 +1,22 @@
+export type ZiiplyStoreDirectoryEntry = {
+  id: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+};
 
+export const ZIIPLY_STORE_DIRECTORY: ZiiplyStoreDirectoryEntry[] = [];
+
+export function normalizeApiStoresToZiiplyDirectory() {
+  return [];
+}
+
+export function resolveZiiplyStoresFromDirectory() {
+  return {
+    nearestStores: [],
+    selectedSLocal: undefined,
+    selectedKLocal: undefined,
+    selectedSHyper: undefined,
+    selectedKHyper: undefined,
+  };
+}
