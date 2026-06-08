78c8c326b9f92fe6047b7a6a277e00d070b74091
ziipli2026 <Niko@trueaim.fi>
2026-06-08 13:10:01 +0300

    Add files via upload

---
+++ src/app/components/ziiply/search/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Ziiply normal product search module barrel.
+ * Location: src/app/components/ziiply/search/index.ts
+ */
+
+export * from "./types";
+export * from "./searchNormalizer";
+export * from "./searchSynonyms";
+export * from "./searchIntentAI";
+export * from "./searchIntentMemory";
+export * from "./searchAutocomplete";
+export * from "./searchRanker";
+export * from "./searchEngine";
