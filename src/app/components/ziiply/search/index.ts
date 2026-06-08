b005ce317e8cc8f39e582a3bb81b36e1bae57d10
ziipli2026 <Niko@trueaim.fi>
2026-06-08 13:45:07 +0300

    Update index.ts

--- src/app/components/ziiply/search/index.ts
+++ src/app/components/ziiply/search/index.ts
@@ -3,9 +3,6 @@ export * from "./searchNormalizer";
 export * from "./searchSynonyms";
 export * from "./searchIntentAI";
 export * from "./searchIntentMemory";
-export {
-  buildZiiplyAutocomplete,
-  buildAutocompleteFromProducts,
-} from "./searchAutocomplete";
+export * from "./searchAutocomplete";
 export * from "./searchRanker";
 export * from "./searchEngine";
